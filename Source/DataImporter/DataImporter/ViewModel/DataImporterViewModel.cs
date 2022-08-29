7ca8beac676adfd001595a64383492cced310348
Ehab Omar <80954084+eomar22@users.noreply.github.com>
2022-08-29 20:00:55 +0200

    Implementing commands

--- Source/DataImporter/DataImporter/ViewModel/DataImporterViewModel.cs
+++ Source/DataImporter/DataImporter/ViewModel/DataImporterViewModel.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using DataImporter.Helpers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 
 namespace DataImporter.ViewModel
 {
@@ -12,6 +15,7 @@ namespace DataImporter.ViewModel
     {
         private string m_ImportFilePath = "Select the file to import...";
         private double m_CellSizeEasting = 0;
+        private double m_CellSizeNorthing = 0;
 
         public string ImportFilePath
         {
@@ -22,7 +26,6 @@ namespace DataImporter.ViewModel
                 NotifyPropertyChanged();
             }
         }
-
         public double CellSizeEasting
         {
             get => m_CellSizeEasting;
@@ -32,12 +35,39 @@ namespace DataImporter.ViewModel
                 NotifyPropertyChanged();
             }
         }
-        public double CellSizeNorthing { get; set; } = 0;
+        public double CellSizeNorthing
+        {
+            get => m_CellSizeNorthing;
+            set
+            {
+                m_CellSizeNorthing = value;
+                NotifyPropertyChanged();
+            }
+        }
         public int InlineNodeCount { get; set; } = 0;
         public int CrossLineNodeCount { get; set; } = 0;
+        public ICommand ImportFileCommand
+        {
+            get
+            {
+                if (m_ImportFileCommand == null)
+                {
+                    return new RelayCommand(p => Import());
+                }
+                return m_ImportFileCommand;
+            }
+        }
+
         public DataImporterViewModel()
         {
 
+        }
+
+        private ICommand m_ImportFileCommand;
+
+        private void Import()
+        {
+            MessageBox.Show($"it works {CellSizeEasting}");
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
